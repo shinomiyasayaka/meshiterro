8b0f052ddbdbfc488018cbde04cba8901966ba8c
shinomiyasayaka <ec2-user@ip-172-31-26-76.ec2.internal>
2024-04-15 13:08:26 +0000

    [Finish] 4-15

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,7 +1,13 @@
 class UsersController < ApplicationController
   def show
+    @user = User.find(params[:id])
+    @post_images = @user.post_images
   end
 
   def edit
+  end
+  
+  def user_params
+    params.require(:user).permit(:id, :profile_image)
   end
 end
