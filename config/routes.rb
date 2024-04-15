04a7733664bfc38d7544fc4b854f9da8022dd244
shinomiyasayaka <ec2-user@ip-172-31-26-76.ec2.internal>
2024-04-15 03:03:11 +0000

    [Finish] 4-9

--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,11 @@ Rails.application.routes.draw do
   
   devise_for :users
   root to: "homes#top"
-  get 'homes/about' => "homes#about"
+  
+  resources :post_images, only: [:new, :index, :show]
+  
+  get "/homes/about" => "homes#about", as: "about"
+  #4-9参照
   
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
