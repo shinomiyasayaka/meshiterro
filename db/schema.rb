2e3bf3997ba12c597a3b40055529e7adbf3de5f7
shinomiyasayaka <ec2-user@ip-172-31-26-76.ec2.internal>
2024-04-15 02:30:43 +0000

    [Finish] 4-8

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2024_04_14_095401) do
+ActiveRecord::Schema.define(version: 2024_04_15_004840) do
 
   create_table "active_storage_attachments", force: :cascade do |t|
     t.string "name", null: false
@@ -41,6 +41,14 @@ ActiveRecord::Schema.define(version: 2024_04_14_095401) do
   end
 
   create_table "meshis", force: :cascade do |t|
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+  end
+
+  create_table "post_images", force: :cascade do |t|
+    t.string "shop_name"
+    t.text "caption"
+    t.integer "user_id"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
   end
